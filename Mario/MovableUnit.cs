4329c4a01f13bcd0896f508cb9f58f4f712358d6
am636e <bnazariy@gmail.com>
2013-09-20 15:26:48 +0300

    solution and other files

---
+++ Mario/MovableUnit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mario
+{
+    public abstract class MovableUnit: Unit, IMovable
+    {
+        public event EventHandler MovedLeft;
+        public event EventHandler MovedRight;
+
+        public MovableUnit(string bitmapPath, int life)
+            :base(bitmapPath)
+        {
+            this.life = life;
+        }
+
+        public MovableUnit(string bitmapPath, int life, System.Drawing.Point p)
+            :base(bitmapPath, life, p )
+        { }
+
+        public void MoveLeft()
+        {
+            this.X += STEP;
+
+            if (MovedLeft != null)
+            {
+                MovedLeft(this, EventArgs.Empty);
+            }
+        }
+
+        public void MoveRight()
+        {
+            this.X -= STEP;
+
+            if (MovedRight != null)
+            {
+                MovedRight(this, EventArgs.Empty);
+            }
+        }
+    }
+}

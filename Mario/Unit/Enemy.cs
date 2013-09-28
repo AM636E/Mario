ce87f0a52a0a110521ca70c5bf0d1f0cf9a2b7f7
am636e <bnazariy@gmail.com>
2013-09-28 10:54:00 +0300

    Enemy class constructors

--- Mario/Unit/Enemy.cs
+++ Mario/Unit/Enemy.cs
@@ -5,4 +5,25 @@ using System.Text;
 
 namespace Mario
 {
+    class Enemy: MovableUnit
+    {
+        public Enemy()
+            : base()
+        { }
+
+        public Enemy(string bitmapPath) :
+            base(bitmapPath)
+        {
+            movers.Add(Mario.MotionState.NotMoving, delegate() { });
+        }
+
+        public Enemy(string bitmapPath, int life)
+            : this(bitmapPath)
+        { this.life = life; }
+
+        public Enemy(string bitmapPath, int life, System.Drawing.Point p)
+            : base(bitmapPath, life)
+        { this.position = p; }
+
+    }
 }
